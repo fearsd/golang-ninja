9e1346ea514ecac2ebcc6d8c5a265a4a25d2f391
fearsd <rifatfazlutdinov@gmail.com>
2023-10-05 17:28:53 +0300

    fibonacci numbers

--- homework/due_10-07-2023/task_1/main.go
+++ homework/due_10-07-2023/task_1/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func fibonacci() func() int {
+	i1 := 0
+	i2 := 1
+	return func() int {
+		sum := i1 + i2
+		i1 = i2
+		i2 = sum
+		return sum
+	}
+}
+
 func main() {
-	fmt.Println("i don't work yet")
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
 }
